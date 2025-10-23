91204eaecd34336afdba491deb58a51460d25678
Valdir Oliveira <whiteofdark@outlook.com>
2025-10-23 01:43:39 -0300

    fix: resolve conflitos de packaging e atualiza teste para product flavors

--- app/src/androidTest/java/com/alunando/morando/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/alunando/morando/ExampleInstrumentedTest.kt
@@ -17,6 +17,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.alunando.morando", appContext.packageName)
+        // Com productFlavors, o package name inclui sufixos do flavor e buildType
+        assert(appContext.packageName.startsWith("com.alunando.morando"))
     }
 }
